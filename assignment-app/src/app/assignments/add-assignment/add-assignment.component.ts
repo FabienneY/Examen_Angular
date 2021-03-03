27b0817a8f1afd348e8d0972cd77df12f638ae04
FabienneY <66945418+FabienneY@users.noreply.github.com>
2021-03-03 18:44:24 +0000

    arrangement_image

--- assignment-app/src/app/assignments/add-assignment/add-assignment.component.ts
+++ assignment-app/src/app/assignments/add-assignment/add-assignment.component.ts
@@ -26,20 +26,13 @@ export class AddAssignmentComponent implements OnInit {
   
   
    matieres: Matiere[] = [
-<<<<<<< HEAD
-    {nomMatiere: 'oracle', imgMatiere: 'assets/images/oracle.jpg', nomProf: 'Mopolo', photoProf: 'assets/images/oracle.jpg'},
-    {nomMatiere: 'Angular', imgMatiere: 'assets/images/angular.png', nomProf: 'Buffa', photoProf: 'assets/images/angular.png'},
-    {nomMatiere: 'Grails', imgMatiere: 'assets/images/grails.png', nomProf: 'Galli', photoProf: 'assets/images/grails.png'},
-    {nomMatiere: 'java', imgMatiere: 'assets/images/java.png', nomProf: 'Amos', photoProf: 'assets/images/java.png'},
-=======
     {nomMatiere: 'oracle', imgMatiere: 'assets/images/oracle.png', nomProf: 'Mopolo', photoProf: 'assets/images/oracle.png'},
     {nomMatiere: 'Angular', imgMatiere: 'assets/images/angular.png', nomProf: 'Buffa', photoProf: 'assets/images/angular.png'},
     {nomMatiere: 'Grails', imgMatiere: 'assets/images/grails.png', nomProf: 'Galli', photoProf: 'assets/images/grails.png'},
     {nomMatiere: 'java', imgMatiere: 'assets/images/java.png', nomProf: 'Amos', photoProf: 'assets/images/java.png'},
-    {nomMatiere: 'Docker', imgMatiere: 'assets/images/docker.png', nomProf: 'Kamagate', photoProf: 'assets/images/java.png'},
-    {nomMatiere: 'Andoid', imgMatiere: 'assets/images/android.png', nomProf: 'Amos', photoProf: 'assets/images/java.png'},
-    {nomMatiere: 'R', imgMatiere: 'assets/images/R.png', nomProf: 'Alison', photoProf: 'assets/images/java.png'},
->>>>>>> 6cb9c001dc6493e00d4c6d9939c6beb845bb2a82
+    {nomMatiere: 'Docker', imgMatiere: 'assets/images/docker.jpg', nomProf: 'Kamagate', photoProf: 'assets/images/docker.jpg'},
+    {nomMatiere: 'Andoid', imgMatiere: 'assets/images/android.png', nomProf: 'Amos', photoProf: 'assets/images/android.png'},
+    {nomMatiere: 'R', imgMatiere: 'assets/images/R.png', nomProf: 'Alison', photoProf: 'assets/images/R.png'},
   ];
 
   constructor(
